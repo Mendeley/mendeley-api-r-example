39ab4f1ec562b4dc9f68b18ccd1cd92e32bfd744
Chris Kilding <user@test>
2016-03-31 13:55:14 +0000

    Simplify example to just use client creds

--- mendeley.R
+++ mendeley.R
@@ -10,10 +10,6 @@ mendeley <- oauth_endpoint(base_url = 'https://api.mendeley.com/oauth', authoriz
 myapp <- oauth_app(appname = 'My app', key = config$clientId, secret = config$clientSecret)
 token <- oauth2.0_token(mendeley, myapp, scope='all')
 
-profile_rsp <- GET('https://api.mendeley.com/profiles/me', config(token = token))
-profile <- fromJSON(rawToChar(content(profile_rsp)))
-message(paste('Hello, ', profile$display_name, '!', sep=''))
-
 doi <- readline('Enter a DOI: ')
 doc_rsp <- GET(paste('https://api.mendeley.com/catalog?view=stats&doi=', curlEscape(doi), sep=''), config(token = token))
 docs <- fromJSON(rawToChar(content(doc_rsp)))
