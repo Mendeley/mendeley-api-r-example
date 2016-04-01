073074cb1ba07c5432ca34985f70928152c89f50
Chris Kilding <user@test>
2016-04-01 10:06:50 +0000

    Patch up GET to catalogue so it works.

--- mendeley.R
+++ mendeley.R
@@ -7,31 +7,45 @@ require(optparse)
 option_list <- list(
   make_option(c("-d", "--doi"), action="store_true", help="DOI to search for")
 )
-opt = parse_args(OptionParser(option_list = option_list))
+program_args = parse_args(OptionParser(option_list = option_list))
+doi <- program_args$doi
 
-clientId <- Sys.getenv("MENDELEY_CLIENT_ID")
-clientSecret <- Sys.getenv("MENDELEY_CLIENT_SECRET")
+message("Obtain OAuth client ID and secret from the corresponding environment variables")
 
-secret = RCurl::base64(paste(clientId, clientSecret, sep = ":"))
+client_id <- Sys.getenv("MENDELEY_CLIENT_ID")
+client_secret <- Sys.getenv("MENDELEY_CLIENT_SECRET")
+basic_auth_credentials = RCurl::base64(paste(client_id, client_secret, sep = ":"))
 
-req = POST('https://api.mendeley.com/oauth/token',
+message("Obtain OAuth access token")
+
+access_token_rsp = POST('https://api.mendeley.com/oauth/token',
 	   add_headers(
-		       'Authorization' = paste("Basic", secret),
+		       'Authorization' = paste("Basic", basic_auth_credentials),
 		       'Content-Type' = 'application/x-www-form-urlencoded'
 	   ),
 	   body = 'grant_type=client_credentials&scope=all'
 )
 
-token = paste("Bearer", content(req)$access_token)
+access_token = content(access_token_rsp)$access_token
 
-doi <- opt$doi 
-doc_rsp <- GET(paste('https://api.mendeley.com/catalog?view=stats&doi=', curlEscape(doi), sep=''), config(token = token))
+message("Search the Mendeley catalogue for the specified DOI")
+
+catalogue_url = paste('https://api.mendeley.com/catalog?view=stats&doi=', curlEscape(doi), sep='')
+doc_rsp <- GET(catalogue_url,
+	       add_headers(
+			   'Authorization' = paste("Bearer", access_token),
+			   'Content-Type' = 'application/vnd.mendeley-document.1+json'
+	       )
+)
+
 docs <- fromJSON(rawToChar(content(doc_rsp)))
 
 if (length(docs) > 0) {
   doc <- docs[[1]]
   message(paste(doc$title, 'has', doc$reader_count, 'readers.'))
   
+  message("Create visual representation of the data")
+
   df <- data.frame(t(data.frame(doc$reader_count_by_academic_status)))
   colnames(df) <- c('readers')
   par(mar=c(5,18,1,1), las=2)
